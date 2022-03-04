3688b214625db01d36b4e7e4b46c9ba9384be663
Olga Akhlamova <olga-akhlamova@yandex.ru>
2022-03-05 00:49:00 +0300

    исправлены ошибки после ревью: добавлены семейство шрифтов для текстовых элементов, картинки кнопок заменены на векторные изображения, правильно подключила шрифт, задала гибкие значения высоты и ширины, добавила в функцию открытия попапа выведение значений полей профиля

--- scripts/main.js
+++ scripts/main.js
@@ -1,29 +1,31 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.popup-open');
-let closePopup = popup.querySelector('.popup-close')
-
-function togglePopup() {
-    popup.classList.toggle('popup__opened');  
-}
-
-openPopup.addEventListener('click', togglePopup);
-
-closePopup.addEventListener('click', togglePopup);
+let openPopup = document.querySelector('.profile__info-edit');
+let closePopup = popup.querySelector('.popup__close');
 
 let profile = document.querySelector('.profile');
 let profileContainer = profile.querySelector('.profile__info');
 let profileName = profile.querySelector('.profile__info-name');
-let profileJob = profile.querySelector('.profile__info-job')
+let profileJob = profile.querySelector('.profile__info-job');
 
 let formElement = popup.querySelector('.popup__container');
-let nameInput = popup.querySelector('.input-name');
-let jobInput = popup.querySelector('.input-job');
+let nameInput = popup.querySelector('.popup__subtitle_name');
+let jobInput = popup.querySelector('.popup__subtitle_job');
+
+function togglePopup() {
+    popup.classList.toggle('popup_opened');
+    nameInput.textContent = profileName;
+    jobInput.textContent = profileJob;  
+}
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
-    popup.classList.remove('popup__opened')
+    popup.classList.remove('popup_opened')
 }
+
+openPopup.addEventListener('click', togglePopup);
+
+closePopup.addEventListener('click', togglePopup);
 
 formElement.addEventListener('submit', formSubmitHandler); 
